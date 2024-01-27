8ebb92d059555d218697edc6b5ea18c659f92521
Noxware <7684329+noxware@users.noreply.github.com>
2024-01-27 17:39:28 -0300

    group my plugins

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,14 @@ mod world;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_plugins(camera::CameraPlugin)
-        .add_plugins(miko::MikoPlugin)
-        .add_plugins(bunny::BunnyPlugin)
-        .add_plugins(enemy::EnemyPlugin)
-        .add_plugins(animation::AnimationPlugin)
-        .add_plugins(world::WorldPlugin)
+        .add_plugins((
+            camera::CameraPlugin,
+            miko::MikoPlugin,
+            bunny::BunnyPlugin,
+            enemy::EnemyPlugin,
+            animation::AnimationPlugin,
+            world::WorldPlugin,
+        ))
         .add_plugins(WorldInspectorPlugin::new())
         .run();
 }
